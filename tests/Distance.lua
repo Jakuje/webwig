77ea48a652ff8ce0fe23d4b465317de8fce2ae62
Jakub Jelen <xjelen06@stud.fit.vutbr.cz>
2013-03-01 19:23:15 +0100

    Wrongly named GetValue method

--- tests/Distance.lua
+++ tests/Distance.lua
@@ -3,23 +3,23 @@
 dist = Wherigo.Distance(10)
 assert(dist.value == 10, "wrong result for default units: \"value\" ")
 assert(dist() == 10, "wrong result for default units \"call\" ")
-assert(dist.getValue() == 10, "wrong result for default units \"getValue()\" ")
-assert(dist.getValue("meters") == 10, "wrong result for default units \"getValue(meters)\" ")
-assert(dist.getValue("m") == 10, "wrong result for default units \"getValue(m)\" ")
+assert(dist.GetValue() == 10, "wrong result for default units \"getValue()\" ")
+assert(dist.GetValue("meters") == 10, "wrong result for default units \"getValue(meters)\" ")
+assert(dist.GetValue("m") == 10, "wrong result for default units \"getValue(m)\" ")
 
 dist = Wherigo.Distance.new(10, "m")
 assert(dist.value == 10, "wrong result for meters: \"value\" ")
 assert(dist() == 10, "wrong result for meters \"call\" ")
-assert(dist.getValue() == 10, "wrong result for meters \"getValue()\" ")
-assert(dist.getValue("meters") == 10, "wrong result for meters \"getValue(meters)\" ")
-assert(dist.getValue("m") == 10, "wrong result for meters \"getValue(m)\" ")
+assert(dist.GetValue() == 10, "wrong result for meters \"getValue()\" ")
+assert(dist.GetValue("meters") == 10, "wrong result for meters \"getValue(meters)\" ")
+assert(dist.GetValue("m") == 10, "wrong result for meters \"getValue(m)\" ")
 
 
 dist = Wherigo.Distance(100, "feet")
-assert(dist.value == 30.48, "wrong result for feets: \"value\" ")
-assert(dist.getValue() == 30.48, "wrong result for feets \"getValue()\" ")
-assert(dist.getValue("meters") == 30.48, "wrong result for feets \"getValue(meters)\" ")
-assert(dist.getValue("feet") == 100, "wrong result for feets \"getValue(feet)\" ")
+assert(dist.Galue == 30.48, "wrong result for feets: \"value\" ")
+assert(dist.GetValue() == 30.48, "wrong result for feets \"getValue()\" ")
+assert(dist.GetValue("meters") == 30.48, "wrong result for feets \"getValue(meters)\" ")
+assert(dist.GetValue("feet") == 100, "wrong result for feets \"getValue(feet)\" ")
 print(dist)
 
 
