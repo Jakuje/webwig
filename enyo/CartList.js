a1923fc9771fbc44337724c080d1402415c7456c
Jakub Jelen <jakuje@gmail.com>
2013-03-08 20:47:24 +0100

    Testing messages

--- enyo/CartList.js
+++ enyo/CartList.js
@@ -35,10 +35,12 @@ enyo.kind({
 
 
    	openError: function() {
-		//this.$.errorMessage.openAtCenter();
-		this.owner.popupMessage("Zprava chybova", "Message", "images/tasks.png", "Dobre", "Spatne", true);
-		this.owner.popupMessage("Je prave zobrazena prvni zprava a tato by mela zustat v zasobniku", "Error", "images/tasks.png", "Good", "Wrong", false);
-		this.owner.popupMessage("JAK HRAT:<BR><BR>V nabidce Tasks (Ukoly) najdete konkretni ukoly. Pro jejich splneni staci priblizit se k hledanemu objektu. Na poradi techto objektu pritom nezalezi.<BR><BR>Vzdy vyckejte na potvrzeni nalezu. Nekdy se muze stat, ze budete muset hledat spravne misto, aby byl nalez zaznamenan a potvrzen.<BR><BR>Pozorne ctete pruvodni texty. Reknou vam, jak postupovat dal. Kazdy dialog hned potvrdte tlacitkem.<BR><BR>Kdybyste si nevedeli rady, prozkoumejte polozky v jednotlivych nabidkach.<BR><BR>Hra se automaticky po splneni kazdeho ukolu uklada.", "Message", "", "", "", false);
+		this.owner.popupMessage( new WIGApp.MessageBox("Zprava chybova", "Message", "images/tasks.png", "Dobre", "Spatne", true) );
+		//this.owner.popupMessage( new WIGApp.Dialog("Je prave zobrazena prvni zprava a tato by mela zustat v zasobniku", "Error", "images/tasks.png") );
+		//this.owner.popupMessage( new WIGApp.Dialog("JAK HRAT:<BR><BR>V nabidce Tasks (Ukoly) najdete konkretni ukoly. Pro jejich splneni staci priblizit se k hledanemu objektu. Na poradi techto objektu pritom nezalezi.<BR><BR>Vzdy vyckejte na potvrzeni nalezu. Nekdy se muze stat, ze budete muset hledat spravne misto, aby byl nalez zaznamenan a potvrzen.<BR><BR>Pozorne ctete pruvodni texty. Reknou vam, jak postupovat dal. Kazdy dialog hned potvrdte tlacitkem.<BR><BR>Kdybyste si nevedeli rady, prozkoumejte polozky v jednotlivych nabidkach.<BR><BR>Hra se automaticky po splneni kazdeho ukolu uklada.", "Message") );
+		this.owner.popupMessage( new WIGApp.GetInput("Zadejte text:", "Input", "images/inventory.png", "Text", ""));
+		
+		this.owner.popupMessage( new WIGApp.GetInput("Vyberte:", "Select", "images/you_see.png", "MultipleChoice", ";one;two;three"));
 	},
 
 	create: function() {
