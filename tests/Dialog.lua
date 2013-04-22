aba65165fcdd5280b7c9e57141353a9fffe51ba9
Jakub Jelen <jakuje@gmail.com>
2013-04-22 16:46:37 +0200

    Litle bit more reasonable

--- tests/Dialog.lua
+++ tests/Dialog.lua
@@ -1,6 +1,13 @@
 
 -- simple message
-Wherigo.Dialog({Text = "Message content"})
+Wherigo.Dialog({Text = "Simple message content"})
+
+-- multiple messages
+Wherigo.Dialog({
+	{Text = "First of multiple"},
+	{Text = "Second text"},
+	{Text = "Last text"}
+})
 
 
 -- with media
@@ -8,4 +15,4 @@ cart = Wherigo.ZCartridge()
 media = Wherigo.ZMedia(cart)
 media.Id = "ca2faabc5164-ca2faabc5164"
 
-Wherigo.MessageBox({Text = "Message content", Media = media})
+Wherigo.MessageBox({Text = "MessageBox content", Media = media})
